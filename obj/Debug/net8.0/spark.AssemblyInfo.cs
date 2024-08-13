ab505afe84945e6acce38890a727151a70d6c071
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-13 10:38:24 -0500

    fixing files

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4edde19d90e8a5be28aa712a7d4dab60071d7a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+016b6352ea6e93e79fa2fc2d7b44979bbe01f42f")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
