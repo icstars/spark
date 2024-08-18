d392053826f0de2472c6ff81fc3c7574af76e28b
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-18 13:17:47 -0500

    login page connected to entity framework and work with database info

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71c25c3d86ea84e7dbcb34ebaf4262fe019b357c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2546170b36f8c341f4be5eb088f22d622c7ab949")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
