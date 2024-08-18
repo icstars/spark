68f02c9242307c721378843b52b40b86b125c0b3
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-18 15:49:49 -0500

    test

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd56c205e51e3559e24f4be565454b21771561ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7afc7feab0ac713a9d5815e3776aefa66838d5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
