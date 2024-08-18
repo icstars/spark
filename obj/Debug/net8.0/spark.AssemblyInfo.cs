f0f1921ac27befa6a1979c349f0456ec2113ddf5
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-18 15:58:38 -0500

    cleaned cache

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbb0b2284d5a49ea07b6d8f1c8b1af61f859d771")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cfd62cee20582193a4bb1ebd50aca930c01b93c")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
