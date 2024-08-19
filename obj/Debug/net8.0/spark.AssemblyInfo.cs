21d80287950c8109929fc8a40d4e53ce666a5b58
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-19 13:48:29 -0500

    merge with people_func

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7afc7feab0ac713a9d5815e3776aefa66838d5a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a65373f7506e7ef38f5b8c293bfc837d373c8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
