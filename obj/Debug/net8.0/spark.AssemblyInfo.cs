4bc6a4a317f3281f422c1088babf3103f677d828
DeeMRoss <deeross2345@gmail.com>
2024-08-21 15:27:17 -0500

    Finish Footer component

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c053fe35c4f395a39e27dd984503b9828a0a1ecc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06a72122b17382a77b15e3b645b623339384322f")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
