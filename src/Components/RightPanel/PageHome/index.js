8c2210125110423b91c47edcfebb3e3672351008
Angel Campos <angelcampos@MacBook-Pro.local>
2024-08-23 15:07:34 -0500

    View Evaluation button completed

--- src/Components/RightPanel/PageHome/index.js
+++ src/Components/RightPanel/PageHome/index.js
@@ -2,7 +2,6 @@ import { Link } from 'react-router-dom';
 import React from 'react';
 function pageHome() {
     return (
-
         <div>
             <div className="right-panel-view-evaluation-button">
                 <Link className="evaluation-button" to="/EvalOverlook"><button type="button">View evaluation</button></Link>
