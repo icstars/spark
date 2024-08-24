3ecea2a1f0c73864c39bb1266ec78a081f1b2b4f
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-24 14:27:34 -0500

    merge with eval_func

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02492e33674ef17d56a74070e86af7c3e98332e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea343a300167f322a277f2ff8da6e93b0aac7921")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
