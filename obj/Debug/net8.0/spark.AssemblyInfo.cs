2e1bc1194138a5299638a7b674419d2380577ac3
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-26 10:43:00 -0500

    merge nav-menu

--- obj/Debug/net8.0/spark.AssemblyInfo.cs
+++ obj/Debug/net8.0/spark.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("spark")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc45eb9a65ae0763c26009f3446812a5bdf63fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+977993c1a22cd07b603011fb3767e117a5612f44")]
 [assembly: System.Reflection.AssemblyProductAttribute("spark")]
 [assembly: System.Reflection.AssemblyTitleAttribute("spark")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
