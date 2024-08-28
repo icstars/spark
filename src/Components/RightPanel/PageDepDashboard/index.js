06863bca436a62c149f9ac977619f901f6a51d76
Valerii Kharchenko <vkharchenko2272@gmail.com>
2024-08-28 14:10:49 -0500

    DeeWork

--- src/Components/RightPanel/PageDepDashboard/index.js
+++ src/Components/RightPanel/PageDepDashboard/index.js
@@ -7,13 +7,25 @@ function pageDepDashboard() {
     return (
         <div>
             <div className="people-rand-list">
-                <p>People</p>
-                <p>Employee Name</p>
+            <h2 className='title-list-people'>Employee</h2> 
+                <ul>
+                <li> Jim Jones 20 </li>
+                <li> Bob Gates 15 </li>
+                <li> Eva Ryan 29 </li>
+                <li> Tom Perez 40 </li>
+                <li> Rita Khan 25 </li>
+                </ul>
                 <Link to="/People"><button type="button">Open more</button></Link>
             </div>
-            <div className="topic-scores">
-                <p>Topic scores</p>
-                <p>Topic name</p>
+            <div>
+                <h3 className="category-scores">Category Scores</h3>
+                <ul>
+                <li>Teamwork 3</li>
+                <li>Communication 1</li>
+                <li>Knowledge Application & Problem Solving 3</li>
+                <li>Code Aesthetics 2</li>
+                <li>Best Practices 3</li>
+                </ul>
             </div>
         </div>
     );
